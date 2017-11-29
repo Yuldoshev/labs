cd4fd76f18ef73efd06915f3b2a92abc5d30f627
Al-Khorazmiy <33149167+uldoshev@users.noreply.github.com>
2017-11-29 11:10:20 +0300

    Лабораторная работа 2
    Разветвляющиеся вычислительные процессы

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,2 +1,20 @@
-//
-//
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    double x,y;
+    cout<<"x=";
+
+if(x>3)
+    y=27*(x-3)*(x-3)*(x-3);
+else if(x>1)
+    y=x*x*x;
+
+else if(x>0)
+    y=x;
+else
+    y=pow(sin(x),2)/2;
+    return 0;
+}
