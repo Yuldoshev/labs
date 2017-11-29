f9ca4782e905f5f57d9e38877f67b7a7a5609187
Al-Khorazmiy <33149167+uldoshev@users.noreply.github.com>
2017-11-29 10:57:52 +0300

    Лабораторная работа 1
    Линейные вычислительные процессы

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
-
+#include<math.h>
+using namespace std;
+///Bu mani birinchi yozgan kodim
 int main()
-{
-  return 0;
+{double x;
+double y;
+cout<<"vvedi x ";
+cin>>x;
+y=(4*sin(x)*sin(x)+3)/2;
+cout<<"x="<<x<<endl;
+  cout<<"y="<<y<<endl;
+
+
+  double c;
+  double g, a,b;
+  a=3.2;
+  b=6.8;
+  g=a*(3*pow(y,2)-1./3);
+  c=pow(sin(g),2);
+  double z=(c+11.75)/(c+b);
+  cout<<"z="<<z<<endl;
+    return 0;
 }
